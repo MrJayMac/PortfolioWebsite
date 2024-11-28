b5ae559b66fe5c34d300fe28929a49a69dd5cbd1
MrJayMac <mac.joshua450@gmail.com>
2024-11-28 08:05:18 -0500

    adding some icons

--- client/src/components/Skills/Skills.jsx
+++ client/src/components/Skills/Skills.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { FaJs, FaPython, FaReact, FaHtml5, FaCss3Alt, FaGit, FaDocker } from 'react-icons/fa';
-import { DiNodejs, DiDjango, DiPostgresql } from 'react-icons/di';
-import { IoLogoGithub, IoLogoWindows } from 'react-icons/io';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact, faJs, faPython, faHtml5, faCss3Alt, faNodeJs, faGit, faDocker, faPython as faDjango } from '@fortawesome/free-brands-svg-icons';
 import './Skills.css';
 
 const Skills = () => {
@@ -15,27 +14,25 @@ const Skills = () => {
           <h3>Languages</h3>
           <div className="skills__content">
             <article className='skills__details'>
-              <FaJs size={30} />
               <div>
+                <FontAwesomeIcon icon={faJs} className="icon" style={{ color: '#F7DF1E' }} />
                 <h4>JavaScript</h4>
               </div>
             </article>
             <article className='skills__details'>
-              <FaPython size={30} />
               <div>
+                <FontAwesomeIcon icon={faPython} className="icon" style={{ color: '#306998' }} />
                 <h4>Python</h4>
               </div>
             </article>
             <article className='skills__details'>
-              <FaReact size={30} />
               <div>
                 <h4>TypeScript</h4>
               </div>
             </article>
             <article className='skills__details'>
-              <IoLogoWindows size={30} />
               <div>
-                <h4>Visual Basic (.NET)</h4>
+                <h4>Visual Basics (.NET)</h4>
               </div>
             </article>
           </div>
@@ -46,33 +43,31 @@ const Skills = () => {
         <div className="skills">
           <h3>Frontend</h3>
           <div className="skills__content">
-            <article className='skills__details'>
-              <FaReact size={30} />
+          <article className='skills__details'>
               <div>
+                <FontAwesomeIcon icon={faReact} className="icon" style={{ color: '#61DAFB' }} />
                 <h4>React</h4>
               </div>
             </article>
             <article className='skills__details'>
-              <FaHtml5 size={30} />
               <div>
+                <FontAwesomeIcon icon={faHtml5} className="icon" style={{ color: '#E34F26' }} />
                 <h4>HTML</h4>
               </div>
             </article>
             <article className='skills__details'>
-              <FaCss3Alt size={30} />
               <div>
+                <FontAwesomeIcon icon={faCss3Alt} className="icon" style={{ color: '#1572B6' }} />
                 <h4>CSS</h4>
               </div>
             </article>
             <article className='skills__details'>
-              <FaGit size={30} />
               <div>
-                <h4>Git</h4>
+                <h4>Bootstrap</h4>
               </div>
             </article>
           </div>
         </div>
-
         {/* End of Frontend */}
 
         {/* Beginning of Backend */}
@@ -80,19 +75,18 @@ const Skills = () => {
           <h3>Backend and Databases</h3>
           <div className="skills__content">
             <article className='skills__details'>
-              <DiNodejs size={30} />
               <div>
+                <FontAwesomeIcon icon={faNodeJs} className="icon" style={{ color: '#8CC84B' }} />
                 <h4>NodeJS</h4>
               </div>
             </article>
             <article className='skills__details'>
-              <DiDjango size={30} />
               <div>
+                <FontAwesomeIcon icon={faDjango} className="icon" style={{ color: '#092E20' }} />
                 <h4>Django</h4>
               </div>
             </article>
             <article className='skills__details'>
-              <DiPostgresql size={30} />
               <div>
                 <h4>PostgreSQL</h4>
               </div>
@@ -104,7 +98,6 @@ const Skills = () => {
             </article>
           </div>
         </div>
-
         {/* End of Backend */}
 
         {/* Beginning of Others */}
@@ -112,22 +105,31 @@ const Skills = () => {
           <h3>Others</h3>
           <div className="skills__content">
             <article className='skills__details'>
-              <FaDocker size={30} />
               <div>
+                <FontAwesomeIcon icon={faGit} className="icon" style={{ color: '#F1502F' }} />
+                <h4>Git</h4>
+              </div>
+            </article>
+            <article className='skills__details'>
+              <div>
+                <FontAwesomeIcon icon={faDocker} className="icon" style={{ color: '#0DB7ED' }} />
                 <h4>Docker</h4>
               </div>
             </article>
             <article className='skills__details'>
-              <IoLogoGithub size={30} />
               <div>
-                <h4>GitHub</h4>
+                <h4>Arduino</h4>
+              </div>
+            </article>
+            <article className='skills__details'>
+              <div>
+                <h4>Jenkins</h4>
               </div>
             </article>
           </div>
         </div>
+        {/* End of Others */}
       </div>
-      {/* End of Others */}
-
     </section>
   );
 }
