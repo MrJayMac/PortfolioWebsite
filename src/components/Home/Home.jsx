5f7803d529219a1fcb269c7a5fe2d8689de1e531
MrJayMac <mac.joshua450@gmail.com>
2025-01-07 15:21:18 -0500

    added resume

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import './Home.css'
-import P2 from '../Photos/P2.png'
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import './Home.css';
+import P2 from '../Photos/P2.png';
+import { FaLinkedin, FaGithub, FaFileDownload } from "react-icons/fa";
+
 const Home = () => {
   return (
     <section id="home">
@@ -12,14 +13,22 @@ const Home = () => {
         <div className="intro__content">
           <h1>Hi, I'm Joshua Mac</h1>
           <p>
-          I am a Computer Engineering graduate from Queen’s University, experienced in full-stack development, database systems, and machine learning. 
-          <div>
-          Here, you’ll find insights into my projects, experiences, and the skills that drive my passion for creating impactful solutions. I’m always excited to connect, collaborate, and explore new opportunities. 
+            I am a Computer Engineering graduate from Queen’s University, experienced in full-stack development, database systems, and machine learning.
+            <div>
+              Here, you’ll find insights into my projects, experiences, and the skills that drive my passion for creating impactful solutions. I’m always excited to connect, collaborate, and explore new opportunities.
             </div>
           </p>
-          <a href="https://www.linkedin.com/in/joshua-mac-280556165/" className="icon"><FaLinkedin/></a>
-          <a href="https://github.com/MrJayMac" className="icon"><FaGithub/></a>
-
+          <div className="icons">
+            <a href="/Resume-Joshua-Mac.pdf" className="icon" download="Resume-Joshua-Mac.pdf">
+              <FaFileDownload />
+            </a>
+            <a href="https://github.com/MrJayMac" className="icon" target="_blank" rel="noopener noreferrer">
+              <FaGithub />
+            </a>
+            <a href="https://www.linkedin.com/in/joshua-mac-280556165/" className="icon" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin />
+            </a>
+          </div>
         </div>
       </div>
     </section>
